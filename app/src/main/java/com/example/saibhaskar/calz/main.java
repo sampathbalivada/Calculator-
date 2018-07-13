561ec83181c7157b04331eee42bc60801a0bad78
saibhaskar24 <saibaaskar24091999@gmail.com>
2018-07-13 23:57:21 +0530

    java updated

--- app/src/main/java/com/example/saibhaskar/calz/main.java
+++ app/src/main/java/com/example/saibhaskar/calz/main.java
@@ -12,7 +12,6 @@ public class main extends AppCompatActivity {
 
     double result = 0;
     String output = "";
-    int[] vals = new int[10];
     TextView answer ;
     EditText out ;
 
@@ -131,6 +130,8 @@ public class main extends AppCompatActivity {
     }
 
      public void equal(View view) {
+         double[] vals = new double[10];
+         double l,m;
         int i = 0, j = 0, k = 0;
         char[] outputs = output.toCharArray();
         char[] oper = new char [10];
@@ -139,15 +140,14 @@ public class main extends AppCompatActivity {
              ++i;
          }
          i=0;
-        String l = "";
         while (i < output.length()) {
-            if (outputs[i] == '.') {
-                l = "0.";
-                while (outputs[i] != '+' || outputs[i] != '-' || outputs[i] != '*' || outputs[i] != '%' || outputs[i] != '/' || i < output.length()) {
-                    l = l + outputs[i];
+            if (outputs[i] == '.') {++i; l = 0;m=0;
+                while (i < output.length() && outputs[i] != '+' && outputs[i] != '-' && outputs[i] != '*' && outputs[i] != '%' && outputs[i] != '/' )  {
+                    ++m;
+                    l = (l * 10) +  Character.getNumericValue(outputs[i]);
                     ++i;
-                }
-                vals[j] = vals[j] + Integer.parseInt(l);
+                }l = l * Math.pow(0.1,m);
+                vals[j] = vals[j] + l;
             }
             if (i < output.length()) {
                 switch (outputs[i]) {
